8f23bd606ca9d47232b192c2f22e6aab391bda4a
Fathoni Adam <fathoniadam933@gmail.com>
2026-02-04 10:06:13 +0700

    update style

--- app/deps.py
+++ app/deps.py
@@ -1,16 +1,11 @@
 from fastapi import Header, HTTPException, status
 
-RAPIDAPI_KEY = "123456"
-
 def verify_rapidapi_key(x_rapidapi_key: str = Header(None)):
     if x_rapidapi_key is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Missing X-RapidAPI-Key header"
         )
-    if x_rapidapi_key != RAPIDAPI_KEY:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Invalid RapidAPI Key"
-        )
+
+    # Jangan cek nilainya, RapidAPI yang urus validasi user
     return True
